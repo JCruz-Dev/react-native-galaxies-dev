0e4b16afda25553d35ef3278af149a9470cefff6
Juan Carlos <juancarloscruz278@gmail.com>
2025-01-12 02:25:08 -0400

    Add Biome configuration and update layout structure
    - Introduced .vscode/settings.json for default formatter settings.
    - Added biome.json for project formatting and linting configurations.
    - Updated package.json with new lint and format scripts, and added biome as a dev dependency.
    - Refactored _layout.tsx for improved readability.

--- expo-router-nav/app/_layout.tsx
+++ expo-router-nav/app/_layout.tsx
@@ -1,10 +1,12 @@
 import { Stack, useRouter } from "expo-router";
 
 const StackLayout = () => {
-    
   return (
-      <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false, headerTitle: "Your Home" }} />
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{ headerShown: false, headerTitle: "Your Home" }}
+      />
       <Stack.Screen name="Home" />
       <Stack.Screen name="profile" />
       <Stack.Screen name="feed" />
